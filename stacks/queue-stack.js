fe94a0b003a50d08e9d41411f02ac422fad1ca36
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-04 18:47:16 -0500

    queue update

--- stacks/queue-stack.js
+++ stacks/queue-stack.js
@@ -1,28 +1,18 @@
 class Queue {
-    queue;
+    #queue;
 
     constructor() {
-        this.queue = [];
+        this.#queue = [];
     }
 
     enqueue(item) {
-        this.queue.push(item);
+        this.#queue.push(item);
     }
 
     dequeue() {
-        this.queue.shift();
+        return this.#queue.shift();
     }
 }
 
 const queue = new Queue();
 
-queue.enqueue(4);
-queue.enqueue(5);
-queue.enqueue(6);
-
-console.log(queue.dequeue()); // prints 4
-console.log(queue.queue);
-console.log(queue.dequeue()); // prints 5
-console.log(queue.queue);
-console.log(queue.dequeue()); // prints 6
-console.log(queue.queue);
